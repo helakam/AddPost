ac4037c79f7f9b0de058cf5770a7201d515e18de
Nimantha815 <56678166+Nimantha815@users.noreply.github.com>
2021-03-26 15:26:49 +0530

    Update post.php

--- post.php
+++ post.php
@@ -6,6 +6,13 @@
   $ftype=$_FILES['image']['type'];
   $type=array("image/jpg","image/png","image/jpeg","image/webp");
   $insert = "insert into post values ( 'NULL' ,  '$post' , '$Folder' )";
-  
+    
+    $f=0;
+               for($a=0;$a<4;$a++)
+               {
+               if($type[$a]==$ftype)
+               {
+                if(mysqli_query($connection, $insert)){
+               $f=1;
 }
 >
