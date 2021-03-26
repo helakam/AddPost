680289a8961059dd147c5e643487fc47f36563bf
Nimantha815 <56678166+Nimantha815@users.noreply.github.com>
2021-03-26 15:26:15 +0530

    Update post.php

--- post.php
+++ post.php
@@ -1,6 +1,11 @@
 <?php
   if (isset($_POST['save'])) {
   $post = $_POST['post'];
+  $img = $_FILES['image']['name'];
+  $Folder = "Add Post/picture/".$img;
+  $ftype=$_FILES['image']['type'];
+  $type=array("image/jpg","image/png","image/jpeg","image/webp");
+  $insert = "insert into post values ( 'NULL' ,  '$post' , '$Folder' )";
   
 }
 >
