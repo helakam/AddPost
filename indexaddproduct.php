328bbe7b7c973faaa3436e93e8a8f46ad4bdea8f
Nimantha815 <56678166+Nimantha815@users.noreply.github.com>
2021-03-27 17:42:02 +0530

    Update indexaddproduct.php

--- indexaddproduct.php
+++ indexaddproduct.php
@@ -1,6 +1,5 @@
+<?php session_start(); ?>
 <!DOCTYPE html> 
-
-
 
 <html>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -12,5 +11,20 @@
      <link type="text/css" rel="stylesheet" href="css\footer.css">
   <title>Add Product</title>
 
-
 <body>
+<?php include('php/header1.php');?>
+<div id="content" >
+  <form action="indexaddproduct.php" method="post" enctype="multipart/form-data" >
+    
+
+    <div class="main">
+      <p class="sign" align="center">Add Your Product</p>
+      <form class="form1" enctype="multipart/form-data">
+      <div class="form-group">
+            
+            <div class="remember12" align="center">
+             <label for="Pname"><b>Product Name</b></label>
+           </div>
+    <input class="un " align="center" type="text" placeholder="Enter Name" name="Pname"  required>
+
+  </div>
