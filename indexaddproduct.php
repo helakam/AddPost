93ffca028ea419ba3dd14957ac2ea0d5ea26dc6c
Nimantha815 <56678166+Nimantha815@users.noreply.github.com>
2021-03-27 15:45:21 +0530

    Create indexaddproduct.php

---
+++ indexaddproduct.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> 
+
+
+
+<html>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="add/add.css">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+   <link type="text/css" rel="stylesheet" href="css\header.css">
+     <link type="text/css" rel="stylesheet" href="css\footer.css">
+  <title>Add Product</title>
+
+
+<body>
