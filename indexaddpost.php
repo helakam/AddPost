544543625580620caaae91ceade9c04dac2fc778
Nimantha815 <56678166+Nimantha815@users.noreply.github.com>
2021-03-27 17:38:49 +0530

    Create indexaddpost.php

---
+++ indexaddpost.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css\header.css">
+  <link rel="stylesheet" type="text/css" href="Add post\style.css">
+  <link rel="stylesheet" type="text/css" href="css\footer.css">
+  <title>Add Post</title>
+</head>
+
